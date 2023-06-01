2ea2d10e47ec642d05dd5bc4aad417f542f8700f
Benito Luongo <beluonve@alumni.uv.es>
2023-06-01 17:47:11 +0200

    added profiles and config files for each file for validador, productor, and consumidor

---
+++ validadores/src/main/java/es/uv/bjtwcam/validadores/config/WebCloudConfiguration.java
@@ -0,0 +1,10 @@
+package es.uv.bjtwcam.validadores.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("cloud")
+public class WebCloudConfiguration {
+    
+}
