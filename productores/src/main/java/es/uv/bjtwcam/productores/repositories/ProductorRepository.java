8056b169b425d25fb0fecf3e30ace42e45e9c535
Benito Luongo <beluonve@alumni.uv.es>
2023-06-01 17:54:22 +0200

    changed from JpaRepository to ReactiveCrudRepository

--- productores/src/main/java/es/uv/bjtwcam/productores/repositories/ProductorRepository.java
+++ productores/src/main/java/es/uv/bjtwcam/productores/repositories/ProductorRepository.java
@@ -1,10 +1,11 @@
 package es.uv.bjtwcam.productores.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import es.uv.bjtwcam.productores.domain.Productor;
 
 //Dudas entre usar ReactiveCrudRepository o JPARepository
-public interface ProductorRepository extends JpaRepository<Productor, Integer> {
+public interface ProductorRepository extends ReactiveCrudRepository<Productor, Integer> {
 
 }
--- validadores/src/main/java/es/uv/bjtwcam/validadores/repositories/ValidadorRepository.java
+++ validadores/src/main/java/es/uv/bjtwcam/validadores/repositories/ValidadorRepository.java
@@ -1,9 +1,10 @@
 package es.uv.bjtwcam.validadores.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import es.uv.bjtwcam.validadores.domain.Validador;
 
-public interface ValidadorRepository extends JpaRepository<Validador, Integer>{
+public interface ValidadorRepository extends ReactiveCrudRepository<Validador, Integer>{
     
 }
