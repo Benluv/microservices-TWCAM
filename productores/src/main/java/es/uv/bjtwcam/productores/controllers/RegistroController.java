ce9e54f840ee5c82cf4a47bf2de8d7bb244bcf77
Benito Luongo <beluonve@alumni.uv.es>
2023-06-02 18:02:36 +0200

    Added endpoints to RegistroController from Productor, requires addition of status code as ResponseEntity later on

--- productores/src/main/java/es/uv/bjtwcam/productores/controllers/RegistroController.java
+++ productores/src/main/java/es/uv/bjtwcam/productores/controllers/RegistroController.java
@@ -1,27 +1,57 @@
 package es.uv.bjtwcam.productores.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import es.uv.bjtwcam.productores.domain.Productor;
+import es.uv.bjtwcam.productores.objects.ProductorDTO;
+import es.uv.bjtwcam.productores.services.AnalyticsService;
 import es.uv.bjtwcam.productores.services.ProductorService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class RegistroController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RegistroController.class);
     
     @Autowired 
     private ProductorService ps;
+
+    @Autowired
+    AnalyticsService analyticsService;
+
+    @GetMapping("/productor/{id}")
+    @Operation(summary="Obtener productor", description="Obtener la informacion de un productor por su id")
+    public String getProductor(@PathVariable(name="id") String id) {
+        log.info("getProductor: " + id);
+        // Update count of user access
+        analyticsService.addUserAccess(id);
+        return "Se obtiene el productor: "+ id;
+    }
+
+    @PostMapping("/productor")
+    @Operation(summary="Crear nuevo productor", description="Solicitud de registro de un nuevo productor (No Auth)")
+    public String createProductor(@RequestBody ProductorDTO productor) {
+        log.info("createProductor: " + productor);
+        // insert productor into db
+        ps.insert(productor);
+        return "Se crea el productor: "+ productor;
+    }
+
+    @PutMapping("/productor/{id}")
+    @Operation(summary="Modificar productor", description="Modificacion de la informacion del productor")
+    public String modifyUser(@PathVariable("id") String id, @RequestBody ProductorDTO productor) {
+        log.info("modifyUser: " + id);
+        // update productor into db
+        ps.update(id, productor);
+        return "Productor modificado correctamente";
+    }
 
     // @PostMapping("productor")
     // @Operation(summary="Crear nuevo productor", description="Solicitud de registro de un nuevo productor (No Auth)")
