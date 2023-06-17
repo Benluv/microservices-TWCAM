67bf3db301edfda376d21691465124fe49697629
Benito Luongo <beluonve@alumni.uv.es>
2023-06-17 16:48:05 +0200

    added tokens to application.properties

--- productores/src/main/java/es/uv/bjtwcam/productores/objects/AuthenticatedProductor.java
+++ productores/src/main/java/es/uv/bjtwcam/productores/objects/AuthenticatedProductor.java
@@ -3,11 +3,11 @@ package es.uv.bjtwcam.productores.objects;
 public class AuthenticatedProductor {
     
     private String nif;
-
+    
     public AuthenticatedProductor(String nif) {
         this.nif = nif;
     }
-    
+
     public String getNif() {
         return nif;
     }
