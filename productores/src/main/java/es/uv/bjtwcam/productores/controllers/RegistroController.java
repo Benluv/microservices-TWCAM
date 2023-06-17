4b3e73c061d5e6968e7e7dd22114d967c2499fd7
Benito Luongo <beluonve@alumni.uv.es>
2023-06-17 17:41:10 +0200

    added databaseInitializer

--- productores/src/main/java/es/uv/bjtwcam/productores/controllers/RegistroController.java
+++ productores/src/main/java/es/uv/bjtwcam/productores/controllers/RegistroController.java
@@ -1,7 +1,5 @@
 package es.uv.bjtwcam.productores.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
