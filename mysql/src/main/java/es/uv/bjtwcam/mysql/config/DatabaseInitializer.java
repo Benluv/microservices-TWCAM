4c9dc09f2bda72e44073dee44afde2ae8deaefc0
Benito Luongo <beluonve@alumni.uv.es>
2023-06-25 19:26:39 +0200

    added docker-compose.yaml file to both mysql and mongo services in order to automatically lift up container with mvn spring-boot:run

--- mysql/src/main/java/es/uv/bjtwcam/mysql/config/DatabaseInitializer.java
+++ mysql/src/main/java/es/uv/bjtwcam/mysql/config/DatabaseInitializer.java
@@ -2,7 +2,6 @@ package es.uv.bjtwcam.mysql.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import es.uv.bjtwcam.mysql.domain.Productor;
@@ -10,6 +9,9 @@ import es.uv.bjtwcam.mysql.repositories.ProductorRepository;
 import es.uv.bjtwcam.mysql.domain.Productor.Estado;
 import es.uv.bjtwcam.mysql.domain.Productor.Tipo;
 
+/*
+ * deprecated
+ */
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
     
